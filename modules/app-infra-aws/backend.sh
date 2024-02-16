31b5ca0f0287ef2a66f072a8c33fa5805a05f2b5
Jacopo Pianigiani <jpianigi@cisco.com>
2024-02-16 14:00:20 +0100

    5

--- modules/app-infra-aws/backend.sh
+++ modules/app-infra-aws/backend.sh
@@ -1,7 +1,7 @@
   
-  sudo ownip=$(ifconfig enX0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
-  sudo echo $ownip > /tmp/address.log
+  ownip=$(ifconfig enX0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+  echo $ownip > /tmp/address.log
   echo "/usr/bin/docker run -e 'HOST_NAME=$ownip -e 'SERVICE_PORT=10000 -p 10000:8080 -d descartesresearch/teastore-registry"  >> /tmp/address.log
-  sudo docker run -e "HOST_NAME=$ownip" -e "SERVICE_PORT=10000" -p 10000:8080 -d descartesresearch/teastore-registry
-  sudo docker run -p 3306:3306 -d descartesresearch/teastore-db
+  docker run -e "HOST_NAME=$ownip" -e "SERVICE_PORT=10000" -p 10000:8080 -d descartesresearch/teastore-registry
+  docker run -p 3306:3306 -d descartesresearch/teastore-db
   
--- modules/app-infra-aws/frontend.sh
+++ modules/app-infra-aws/frontend.sh
@@ -1,14 +1,14 @@
   
-  sudo ownip=$(ifconfig enX0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
-  sudo backendip=$1
-  sudo echo " BackendIP is $backendip \n" > /tmp/addresses.log
-  sudo echo "HostIP is $ownip" >> /tmp/addresses.log
+  ownip=$(ifconfig enX0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+  backendip=$1
+  echo " BackendIP is $backendip \n" > /tmp/addresses.log
+  echo "HostIP is $ownip" >> /tmp/addresses.log
   #$(ifconfig enX0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')" >/tamp/addresses.log
 
-  sudo docker run -e "REGISTRY_HOST=$backendip" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ownip" -e "SERVICE_PORT=1111" -e "DB_HOST=$backendip" -e "DB_PORT=3306" -p 1111:8080 -d descartesresearch/teastore-persistence
-  sudo docker run -e "REGISTRY_HOST=$backendip" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ownip" -e "SERVICE_PORT=2222" -p 2222:8080 -d descartesresearch/teastore-auth
-  sudo docker run -e "REGISTRY_HOST=$backendip" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ownip" -e "SERVICE_PORT=3333" -p 3333:8080 -d descartesresearch/teastore-recommender
-  sudo docker run -e "REGISTRY_HOST=$backendip" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ownip" -e "SERVICE_PORT=4444" -p 4444:8080 -d descartesresearch/teastore-image
-  sudo docker run -e "REGISTRY_HOST=$backendip" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ownip" -e "SERVICE_PORT=8080" -p 8080:8080 -d descartesresearch/teastore-webui
+  docker run -e "REGISTRY_HOST=$backendip" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ownip" -e "SERVICE_PORT=1111" -e "DB_HOST=$backendip" -e "DB_PORT=3306" -p 1111:8080 -d descartesresearch/teastore-persistence
+  docker run -e "REGISTRY_HOST=$backendip" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ownip" -e "SERVICE_PORT=2222" -p 2222:8080 -d descartesresearch/teastore-auth
+  docker run -e "REGISTRY_HOST=$backendip" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ownip" -e "SERVICE_PORT=3333" -p 3333:8080 -d descartesresearch/teastore-recommender
+  docker run -e "REGISTRY_HOST=$backendip" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ownip" -e "SERVICE_PORT=4444" -p 4444:8080 -d descartesresearch/teastore-image
+  docker run -e "REGISTRY_HOST=$backendip" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ownip" -e "SERVICE_PORT=8080" -p 8080:8080 -d descartesresearch/teastore-webui
 
   
