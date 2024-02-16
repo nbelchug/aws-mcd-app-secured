7e786b39240f15f57763eb6d9859b4e7064682a2
Jacopo Pianigiani <jpianigi@cisco.com>
2024-02-16 19:05:53 +0100

    12

--- modules/provision-mcd/main.tf
+++ modules/provision-mcd/main.tf
@@ -2,16 +2,6 @@
 # be_nodes =module.app-infra-aws.fe-be_nodes.value 
 
 
-resource "ciscomcd_service_vpc" "aws_mcd_demo_teashop_service_vpc" {
-  name               = "aws-mcd_demo_teashop_service-vpc"
-  csp_account_name   = "aws-account1"
-  region             = "us-east-1"
-  cidr               = "10.0.0.0/23"
-  availability_zones = ["us-east-1a", "us-east-1b"]
-  transit_gateway_id = "tgw-12345678912345678"
-  use_nat_gateway    = true
-}
-
 
     
 
