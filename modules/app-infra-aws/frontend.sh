529fd0b21e655d33fe0a41642a8b72f596efdda7
Jacopo Pianigiani <jpianigi@cisco.com>
2024-02-16 12:53:12 +0100

    aaba

--- modules/app-infra-aws/frontend.sh
+++ modules/app-infra-aws/frontend.sh
@@ -2,7 +2,7 @@
   ownip=$(ifconfig enX0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
   backendip=$1
   echo " BackendIP is $backendip" > /tmp/addresses.log
-  echo "HostIP is $ownip"> /tmp/addresses.log
+  echo "HostIP is $ownip">> /tmp/addresses.log
   #$(ifconfig enX0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')" >/tamp/addresses.log
 
   docker run -e "REGISTRY_HOST=$backendip" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ownip" -e "SERVICE_PORT=1111" -e "DB_HOST=$backendip" -e "DB_PORT=3306" -p 1111:8080 -d descartesresearch/teastore-persistence
