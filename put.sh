b59290203475df8e0315011d56139d6b7c89857b
Jacopo Pianigiani <jpianigi@cisco.com>
2024-02-17 09:31:37 +0100

    CODE UPDATE AT

--- put.sh
+++ put.sh
@@ -3,4 +3,8 @@ git add .
 git status
 git commit -m" CODE UPDATE AT  $NOW"
 git push --force-with-lease origin HEAD
+cd ../aws-exec/
+clear
+echo " ---------------------- EXECUTION DIRECTORY --------------------------------"
+
 
