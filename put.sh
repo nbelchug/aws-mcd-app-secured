e4415c83bd234f835879bec6a4c4bc3f148faa47
Jacopo Pianigiani <jpianigi@cisco.com>
2024-02-17 19:04:50 +0100

    Sat Feb 17 19:04:50 CET 2024

--- put.sh
+++ put.sh
@@ -1,10 +1,10 @@
 echo "----------------------- DEVELOPMENT DIRECTORY -----------------------------"
 echo " Updating repository..."
 
-NOW = date
+NOW = $(date)
 git add .
 git status
-git commit -m" CODE UPDATE AT  $NOW"
+git commit -m "$(date)"
 git push --force-with-lease origin HEAD
 
 clear
