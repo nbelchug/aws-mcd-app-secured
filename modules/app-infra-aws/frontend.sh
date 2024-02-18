bfbf5f2da56d78892adc30e2df377135c0307f36
Jacopo Pianigiani <jpianigi@cisco.com>
2024-02-18 18:27:08 +0100

    Sun Feb 18 18:27:08 CET 2024

--- modules/app-infra-aws/frontend.sh
+++ modules/app-infra-aws/frontend.sh
@@ -6,7 +6,7 @@
 
   echo "1st cmd>  docker run -e 'REGISTRY_HOST=$backendip' -e 'REGISTRY_PORT=10000' -e 'HOST_NAME=$ownip' -e 'SERVICE_PORT=1111' -e 'DB_HOST=$backendip' -e 'DB_PORT=3306' -p 1111:8080 -d descartesresearch/teastore-persistence" >> /tmp/address.log
   echo whoami >> /tmp/address.log
-  docker run -e "REGISTRY_HOST=$backendip" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ownip" -e "SERVICE_PORT=1111" -e "DB_HOST=$backendip" -e "DB_PORT=3306" -p 1111:8080 -d descartesresearch/teastore-persistence
+  sudo docker run -e "REGISTRY_HOST=$backendip" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ownip" -e "SERVICE_PORT=1111" -e "DB_HOST=$backendip" -e "DB_PORT=3306" -p 1111:8080 -d descartesresearch/teastore-persistence
   docker run -e "REGISTRY_HOST=$backendip" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ownip" -e "SERVICE_PORT=2222" -p 2222:8080 -d descartesresearch/teastore-auth
   docker run -e "REGISTRY_HOST=$backendip" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ownip" -e "SERVICE_PORT=3333" -p 3333:8080 -d descartesresearch/teastore-recommender
   docker run -e "REGISTRY_HOST=$backendip" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ownip" -e "SERVICE_PORT=4444" -p 4444:8080 -d descartesresearch/teastore-image
