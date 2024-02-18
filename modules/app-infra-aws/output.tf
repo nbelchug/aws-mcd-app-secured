e021c60401074cf676fd67be66bf018638498cab
Jacopo Pianigiani <jpianigi@cisco.com>
2024-02-18 10:44:47 +0100

    Sun Feb 18 10:44:47 CET 2024

--- modules/app-infra-aws/output.tf
+++ modules/app-infra-aws/output.tf
@@ -43,7 +43,7 @@ output "backend-nodes-public" {
 
 output "transit-gateway-id"{
     description = "id of the tgw of the application in use "
-    value = aws_ec2_transit_gateway.id
+    value = aws_ec2_transit_gateway.fe-be-tgw.id
 }
 
 
