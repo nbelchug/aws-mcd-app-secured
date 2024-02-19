f38122b48bedb9038c36f2e944b27a520eadc6a4
Jacopo Pianigiani <jpianigi@cisco.com>
2024-02-19 19:49:57 +0100

    Mon Feb 19 19:49:57 CET 2024

--- modules/app-instances/backend.sh
+++ modules/app-instances/backend.sh
@@ -2,10 +2,6 @@
   ownip=$(ip address show eth0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
   echo "OWNIP = $ownip" > /tmp/address.log
 
-
-
-
-
   echo "docker run -e 'HOST_NAME=$ownip -e 'SERVICE_PORT=10000 -p 10000:8080 -d descartesresearch/teastore-registry"  >> /tmp/address.log
   docker run -e "HOST_NAME=$ownip" -e "SERVICE_PORT=10000" -p 10000:8080 -d descartesresearch/teastore-registry
   echo "docker run -p 3306:3306 -d descartesresearch/teastore-db" >> /tmp/address.log
