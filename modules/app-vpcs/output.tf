9ec279b1f8a05d3d02a3d764159d93bc72a42827
Jacopo Pianigiani <jpianigi@cisco.com>
2024-02-19 08:39:22 +0100

    Mon Feb 19 08:39:22 CET 2024

--- modules/app-vpcs/output.tf
+++ modules/app-vpcs/output.tf
@@ -5,3 +5,19 @@ output "app_private_subnet_id"{
 output "app_public_subnet_id"{
     value = aws_subnet.public_subnet
 }
+
+output "app_fe_vpc_id" {
+    value = aws_vpc.custom_vpc_fe.id
+}
+
+output "app_be_vpc_id" {
+    vaslue = aws_vpc.custom_vpc_be.id
+}
+
+output "app_fe_cidr_block"{
+    value = aws_vpc.custom_vpc_fe.cidr_block
+}
+
+output "app_be_cidr_block"{
+    value = aws_vpc.custom_vpc_be.cidr_block
+}
