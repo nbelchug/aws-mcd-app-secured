3c8ba13ac63030332db0dcaacf08d14812557247
Jacopo Pianigiani <jpianigi@cisco.com>
2024-02-19 20:09:56 +0100

    Mon Feb 19 20:09:56 CET 2024

--- modules/app-instances/backend.sh
+++ modules/app-instances/backend.sh
@@ -1,6 +1,8 @@
   
   ownip=$(ip address show eth0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
   echo "OWNIP = $ownip" > /tmp/address.log
+  which docker >>address.log
+  PATH = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
 
   echo "docker run -e 'HOST_NAME=$ownip -e 'SERVICE_PORT=10000 -p 10000:8080 -d descartesresearch/teastore-registry"  >> /tmp/address.log
   docker run -e "HOST_NAME=$ownip" -e "SERVICE_PORT=10000" -p 10000:8080 -d descartesresearch/teastore-registry
