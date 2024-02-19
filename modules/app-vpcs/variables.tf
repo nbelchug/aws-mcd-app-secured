44e317da178a462d06a268d24c1aaed5dbabe86b
Jacopo Pianigiani <jpianigi@cisco.com>
2024-02-19 10:37:11 +0100

    Mon Feb 19 10:37:11 CET 2024

---
+++ modules/app-vpcs/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_cidr_frontend"{
+    type = string
+}
+variable "vpc_cidr_backend"{
+    type = string
+}
+
+variable "application_name"{
+    type = string
+}
+
+variable "environment"{
+    type = string
+}
+
