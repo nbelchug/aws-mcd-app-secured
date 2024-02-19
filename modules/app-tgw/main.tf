fa4f2ee7ce29b4b1de233f5b2f10ae142d31a079
Jacopo Pianigiani <jpianigi@cisco.com>
2024-02-19 10:30:10 +0100

    Mon Feb 19 10:30:10 CET 2024

--- modules/app-tgw/main.tf
+++ modules/app-tgw/main.tf
@@ -1,18 +1,4 @@
-# --- root/Terraform_projects/terraform_two_tier_architecture/main.tf
-
-# ************************************************************
-# Description: two-tier architecture with terraform
-# - file name: main.tf
-# - custom VPC
-# - 1 public subnets in different AZs for high availability
-# - 1 private subnets in different AZs
-# - 1 EC2 t2.micro instance in each public subnet
-# ************************************************************
-
-# PROVIDER BLOCK
-
-# VPC BLOCK
-# creating VPC
+# TRANSIT GATEWAY MODULE 
 
 
 terraform {
--- modules/app-tgw/variables.tf
+++ modules/app-tgw/variables.tf
@@ -1,20 +1,20 @@
-# --- root/Terraform_projects/terraform_two_tier_architecture/variables.tf
+#Transit Gateway Module - Variables passed from ROOT module
 variable "app_fe_vpc_id" {
     type = string
 }
-variable "app_be_vpc_id"{
+variable "app_be_vpc_id" {
   type = string
 }
-variable "app_private_subnet_id"{
+variable "app_private_subnet_id" {
   type = string
 }
-variable "app_public_subnet_id "{
+variable "app_public_subnet_id " {
   type = string
 }
-variable "app_fe_cidr_block "{
+variable "app_fe_cidr_block" {
   type = string
 }
-variable "app_be_cidr_block "{
+variable "app_be_cidr_block" {
   type = string
 }
 
