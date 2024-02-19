22362ac8b83fa052d8826cf17bd3d58ca8b9b8e3
Jacopo Pianigiani <jpianigi@cisco.com>
2024-02-19 19:11:19 +0100

    Mon Feb 19 19:11:19 CET 2024

--- modules/provision-mcd/main.tf
+++ modules/provision-mcd/main.tf
@@ -32,10 +32,14 @@ resource "ciscomcd_service_vpc" "aws_service_vpc" {
 #   }
 }
 
-resource "ciscomcd_spoke_vpc" "ciscomcd_spoke_" {
-  count = var.app_vpcs.length
+resource "ciscomcd_spoke_vpc" "ciscomcd_spoke_1" {
+
   service_vpc_id = ciscomcd_service_vpc.aws_service_vpc.id
   spoke_vpc_id   = var.app_fe_vpc_id
 }
 
+resource "ciscomcd_spoke_vpc" "ciscomcd_spoke_2" {
 
+  service_vpc_id = ciscomcd_service_vpc.aws_service_vpc.id
+  spoke_vpc_id   = var.app_be_vpc_id
+}
